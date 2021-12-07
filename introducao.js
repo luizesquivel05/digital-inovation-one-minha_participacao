cab3e9163fc7ff1d5760d09fea05fcb73582c507
luizesquivel05 <luizeduardpontesesquivel@gmail.com>
2021-12-07 14:37:15 -0300

    renovacao do site

---
+++ introducao.js
@@ -0,0 +1,8 @@
+// O código a seguir deseja servir para Educação Financeira em uma loja on-line.
+function valor_produto ( preco, desconto ) {
+    var total = preco*desconto;
+
+    console.log ( total );
+}
+
+valor_produto (20, 0.20);
